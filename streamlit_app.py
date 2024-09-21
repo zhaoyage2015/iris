ca0d480aceec67f11f5928f5836d932b33d934b5
zhaoyage <zhaoyage2015@outlook.com>
2024-09-21 20:54:55 +0800

    second

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,25 +19,23 @@ petal_length = st.number_input('Petal length:', min_value=1.0, max_value=7.0, va
 petal_width = st.number_input('Petal width:', min_value=0.1, max_value=2.5, value=0.2)
 
 
-input_data = np.array([[sepal_length, sepal_width, petal_length, petal_width]])
-
+feature_values = [sepal_length, sepal_width, petal_length, petal_width]
+features = np.array([feature_values])
 
 if st.button("Predict"):
-    prediction = model.predict(input_data)
-    probability = model.predict_proba(input_data)
+    prediction = model.predict(features)[0]
+    probability = model.predict_proba(features)[0]
 
-    st.write(f"**Predicted class:** {prediction[0]}")
-    st.write(f"**Prediction probability:** {probability[0]}")
-
-    
-def st_shap(plot, height=None):
-    shap_html = f"<head>{shap.getjs()}</head><body>{plot.html()}</body>"
-    components.html(shap_html, height=height)
+    st.write(f"**Predicted class:** {prediction}")
+    st.write(f"**Prediction probability:** {probability}")
 
 
-explainer = shap.TreeExplainer(model)
-shap_values = explainer.shap_values(input_data)
+    explainer = shap.TreeExplainer(model)
+    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_names))
 
+    st.header("SHAP Force Plot")
 
-st.header("SHAP Force Plot")
-st_shap(shap.force_plot(explainer.expected_value, shap_values[0], input_data))
+    shap.force_plot(explainer.expected_value, shap_values[0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)
+    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
+    st.image("shap_force_plot.png")
+
